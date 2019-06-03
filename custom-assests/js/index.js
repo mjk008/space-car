38f2c049c3553f53bb21b4cd7ab647bd05409956
mjk008 <41113254+mjk008@users.noreply.github.com>
2019-06-03 14:42:04 +0530

    initial commit

---
+++ custom-assests/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+console.log("document is ready");
+  
+  
+// document ready  
+});
+
+
+$(window).load(function() {
+ // executes when complete page is fully loaded, including all frames, objects and images
+console.log("window is loaded");
+  
+  
+// window load  
+});
